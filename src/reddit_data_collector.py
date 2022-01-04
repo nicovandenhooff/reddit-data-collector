2986fb7f5821a563b42689cdb8a78ec35c4d1c31
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-01-04 15:04:52 -0800

    feat: add full access option to constructor

--- src/reddit_data_collector.py
+++ src/reddit_data_collector.py
@@ -5,11 +5,15 @@ from .exceptions import SubredditError
 
 
 class DataCollector:
-    def __init__(self, client_id, client_secret, user_agent):
+    def __init__(
+        self, client_id, client_secret, user_agent, username=None, password=None
+    ):
         self.reddit = praw.Reddit(
             client_id=client_id,
             client_secret=client_secret,
             user_agent=user_agent,
+            username=username,
+            password=password,
         )
 
     def get_data(
