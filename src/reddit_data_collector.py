6af22942ecea853af5f5db24b952382820169a22
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-01-05 10:30:25 -0800

    feat: add function to update existing data

--- src/reddit_data_collector.py
+++ src/reddit_data_collector.py
@@ -54,6 +54,19 @@ class DataCollector:
         else:
             return pd.concat(dfs.values(), ignore_index=True)
 
+    def update_data(self, csv_path, df, key="id", sort="subreddit_name", save=False):
+        old_df = pd.read_csv(csv_path)
+        new_df = (
+            pd.concat([old_df, df], ignore_index=True)
+            .drop_duplicates(subset=[key], ignore_index=True)
+            .sort_values(sort, ignore_index=True)
+        )
+
+        if save:
+            new_df.to_csv(csv_path, index=False)
+
+        return new_df
+
     # ------------------------------HELPER FUNCTIONS------------------------------ #
 
     def _verify_subreddits(self, subreddits):
